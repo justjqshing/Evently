e8b026a8464b766f77a055b450ffe20131c47e5d
Josh Shire <joshshirechildren@gmail.com>
2024-05-15 14:37:41 -0600

    set up event submission

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,13 +1,21 @@
 import { SearchParamProps } from '@/types'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { getEventById } from '@/lib/actions/event.actions'
 import moment from 'moment-timezone';
 
-const EventDetails = async ({ params: { id } }: SearchParamProps) => {
-  console.log(id)
-  const event = await getEventById(id);
-  const date = moment(event.startDateTime).tz(moment.tz.guess()).format('LLLL');
-  
+const EventDetails = ({ params: { id } }: SearchParamProps) => {
+  const [date, setDate] = useState('');
+
+  useEffect(() => {
+    const fetchEventAndSetDate = async () => {
+      const event = await getEventById(id);
+      const formattedDate = moment(event.startDateTime).tz(moment.tz.guess()).format('LLLL');
+      setDate(formattedDate);
+    };
+
+    fetchEventAndSetDate();
+  }, [id]);
+
   return (
     <div>{date}</div>
   )
