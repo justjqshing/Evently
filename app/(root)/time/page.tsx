de0f8bd24efa4a2d50837e10e560f69135e9ded0
Josh Shire <joshshirechildren@gmail.com>
2024-05-17 13:42:44 -0600

    set up tiemzone

--- app/(root)/time/page.tsx
+++ app/(root)/time/page.tsx
@@ -1,14 +1,37 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { getEventById } from '@/lib/actions/event.actions';
 
-const time = () => {
-    const D = new Date()
-  let diff = D.getTimezoneOffset()
-  console.log(D)
-  console.log(diff)
-  return (
-    <div>{diff}</div>
-  )
-}
+const Time = () => {
+  const [formattedTime, setFormattedTime] = useState('');
 
-export default time
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const event = await getEventById('66477d845e28a69c8e75dea1');
+
+        const startTimeString = ('');
+        const eventDate = new Date('2024-05-18T20:00:00.000Z');
+
+        // Correct options for toLocaleTimeString
+        const options: Intl.DateTimeFormatOptions = {
+          hour: 'numeric',  
+          minute: 'numeric', 
+          hour12: true       
+        };
+        const formattedTime = eventDate.toLocaleTimeString(undefined, options);
+
+        setFormattedTime(formattedTime);
+      } catch (error) {
+        console.error("Error fetching or parsing event time:", error);
+        setFormattedTime("Error loading time");
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <div>{formattedTime}</div>;
+};
+
+export default Time;
