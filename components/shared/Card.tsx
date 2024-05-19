89b98cd1cb4b3a59b0b58f874adc223552731414
Josh Shire <joshshirechildren@gmail.com>
2024-05-19 09:21:27 -0600

    Cards on Homepage

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { IEvent } from '@/lib/database/models/event.model'
 import { formatDateTime } from '@/lib/utils'
 import { auth } from '@clerk/nextjs/server'
@@ -10,75 +11,14 @@ type cardProps = {
     hidePrice: boolean
 }
 const Card = async ({ event, hasOrderLink, hidePrice }: cardProps) => {
-    const { sessionClaims } = auth();
-    const userId = sessionClaims?.userId as string;
-    async function getTimeOffset() {
-        const response = await fetch('http://localhost:3000/api/time');
-      
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-      
-        const data = await response.json();
-        console.log(data.timezoneOffset);
-      }
-      
-      getTimeOffset().catch(e => console.log('There was a problem with your fetch operation: ' + e.message));
-  
-    const isEventCreator = userId === event.organizer._id.toString();
-  
+    console.log(event)
+    const time = new Date()
+    const timezoneOffset = time.getTimezoneOffset()
+    console.log(timezoneOffset)
     return (
-      <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
-        <Link 
-          href={`/events/${event._id}`}
-          style={{backgroundImage: `url(${event.imageUrl})`}}
-          className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
-        />
+        <div>
 
-  
-        {isEventCreator && !hidePrice && (
-          <div className="absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all">
-            <Link href={`/events/${event._id}/update`}>
-              <Image src="/assets/icons/edit.svg" alt="edit" width={20} height={20} />
-            </Link>
-  
-          </div>
-        )}
-  
-        <div
-          className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"
-        > 
-         {!hidePrice && <div className="flex gap-2">
-            <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
-              {event.isFree ? 'FREE' : `$${event.price}`}
-            </span>
-            <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
-              {event.category.name}
-            </p>
-          </div>}
-  
-          <p className="p-medium-16 p-medium-18 text-grey-500">
-            {formatDateTime(event.startDateTime).dateTime}
-          </p>
-  
-          <Link href={`/events/${event._id}`}>
-            <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">{event.title}</p>
-          </Link>
-  
-          <div className="flex-between w-full">
-            <p className="p-medium-14 md:p-medium-16 text-grey-600">
-              {event.organizer.firstName} {event.organizer.lastName}
-            </p>
-  
-            {hasOrderLink && (
-              <Link href={`/orders?eventId=${event._id}`} className="flex gap-2">
-                <p className="text-primary-500">Order Details</p>
-                <Image src="/assets/icons/arrow.svg" alt="search" width={10} height={10} />
-              </Link>
-            )}
-          </div>
         </div>
-      </div>
     )
   }
 
