0a4782f0ee0a636e83cc329da336edad0ca664d5
Josh Shire <joshshirechildren@gmail.com>
2024-05-21 21:35:43 -0600

    fixed no close on redirect

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,6 +1,7 @@
-
+'use client'
 import {
     Sheet,
+    SheetClose,
     SheetContent,
     SheetDescription,
     SheetHeader,
@@ -11,21 +12,57 @@ import { auth } from "@clerk/nextjs/server"
 import Image from "next/image"
 import { Separator } from "../ui/separator"
 import Navitems from "./Navitems"
-const MobileNav = () => {
-  const { sessionClaims } = auth()
+import { useEffect, useState } from "react"
+import { X } from "lucide-react"
+import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
+import { setISODay } from "date-fns"
 
-    const userId = sessionClaims?.userId as string
+
+const MobileNav = () => {
+  const router = useRouter();
+  const pathname = usePathname()
+  const [isOpen, setIsOpen] = useState(false);
+  const [userId, setUserId] = useState(null);
+  
+  useEffect(() => {
+    const fetchAuthData = async () => {
+      try {
+        const res = await fetch('/api/webhook/clerk');
+        if (!res.ok) {
+          throw new Error(`Error fetching user ID: ${res.status} ${res.statusText}`);
+        }
+        const data = await res.json();
+        setUserId(data.userId);
+      } catch (error) {
+        console.error("Error fetching auth data:", error);
+      }
+    };
+
+    fetchAuthData();
+  }, []);
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+  
   return (
     <nav className="md:hidden">
-        <Sheet>
+        <Sheet open={isOpen}>
             <SheetTrigger className='align-middle'>
-                <Image src='/assets/icons/menu.svg' height={24} width={24} alt="Dropdown Icon"/>
+                <Image src='/assets/icons/menu.svg' height={24} width={24} alt="Dropdown Icon" onClick={() => setIsOpen(true)}/>
             </SheetTrigger>
+            
             <SheetContent className="flex flex-col gap-6 bg-white md:hidden ">
+              <SheetClose className="w-full absolute top-7 left-[91%] flex">
+                  <X className="h-5 w-5" onClick={() => setIsOpen(false)}/>
+              </SheetClose>
                <Image src='/assets/images/logo.svg' width={128} height={38} alt="Logo"/>
                <Separator className="border border-gray-50"/>
                <Navitems userId={userId}/>
+
             </SheetContent>
+
+            
             
         </Sheet>
 
--- components/shared/Navitems.tsx
+++ components/shared/Navitems.tsx
@@ -3,22 +3,31 @@ import { headerLinks } from '@/constants'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
+import { SheetClose, Sheet } from '../ui/sheet'
 
 const NavItems = (userId: any) => {
   const pathname = usePathname()
 
 
   return (
-    <ul className='md:flex-between flex w-full md:flex-row flex-col gap-5 justify-center'>
+
+      <ul className='md:flex-between flex w-full md:flex-row flex-col gap-5 justify-center'>
       {headerLinks.map((link) => {
         const isactive = pathname === link.route
         return (
-        <li key={link.label} className={`${isactive && 'text-primary-500 '}`}>
-          <Link href={link.label != 'My Profile' ? link.route : `/${userId.userId}/profile/`} className={`${isactive && 'border-b-2 border-primary-500'}`}>{link.label}</Link>
-        </li>
+
+            <li key={link.label} className={`${isactive && 'text-primary-500 '}`} >
+              <Link href={link.label != 'My Profile' ? link.route : `/${userId.userId}/profile/`} className={`${isactive && 'border-b-2 border-primary-500'}`}>{link.label}</Link>
+            </li>
+
+
+        
         )
   })}
     </ul>
+
+
+    
   )
 }
 
